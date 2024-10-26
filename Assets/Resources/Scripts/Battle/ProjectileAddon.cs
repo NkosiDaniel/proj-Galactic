44c773d0fc6d6fab1aa37f4bfd85a5741d62c628
Ayinde Abrams <xxxenouniverse@gmail.com>
2024-10-26 14:56:34 -0400

    Programmed Pragma's barrage functionality

--- Assets/Resources/Scripts/Battle/ProjectileAddon.cs
+++ Assets/Resources/Scripts/Battle/ProjectileAddon.cs
@@ -8,12 +8,9 @@ public class ProjectileAddon : MonoBehaviour
     [SerializeField] private GameObject explosionPrefab;
 
     private void OnCollisionEnter(Collision other) {
-        if(other.gameObject)
-        {
             if(explosionPrefab != null) {
             GameObject explosion = Instantiate(explosionPrefab, this.transform);
             Destroy(explosion, 1.5f);
-            }
         }
     }
 }

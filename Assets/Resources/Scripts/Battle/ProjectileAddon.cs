9da57f3aef50069f7ef0a34c0b7d562242d74059
Ayinde Abrams <xxxenouniverse@gmail.com>
2024-10-26 22:56:21 -0400

    ALOT of Changes

--- Assets/Resources/Scripts/Battle/ProjectileAddon.cs
+++ Assets/Resources/Scripts/Battle/ProjectileAddon.cs
@@ -6,7 +6,12 @@ public class ProjectileAddon : MonoBehaviour
 {
     [SerializeField] private int damage;
     [SerializeField] private GameObject explosionPrefab;
-
+    [SerializeField] private string soundToPlay;
+    
+    private void Awake() 
+    {
+        FindObjectOfType<AudioManager>().PlaySound(soundToPlay);
+    }
     private void OnCollisionEnter(Collision other) {
             if(explosionPrefab != null) {
             GameObject explosion = Instantiate(explosionPrefab, this.transform);

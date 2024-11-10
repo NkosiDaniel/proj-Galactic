fc5fe22b9a8184c14ed17d6be2be81d9a5373d4b
Ayinde Abrams <xxxenouniverse@gmail.com>
2024-11-09 21:41:24 -0500

    Merging UltimateAttack.cs

--- Assets/Resources/Scripts/Player/UltimateAttack.cs
+++ Assets/Resources/Scripts/Player/UltimateAttack.cs
@@ -3,14 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BeamAttack : UltimateAttack
-{
-    [Serialilzefield] private GameObject projectiles;
-    private override void Execute()
-    {
-        Debug.Log("BeamAttack Attck has Executed");
 public class UltimateAttack : ScriptableObject
 {
+    [SerializeField] private String name;
+    [SerializeField] private String description;
+    [SerializeField] private float activationTime;
+    [SerializeField] private float cooldownTime;
+    [SerializeField] private int damage;
     protected virtual void Execute() 
     {
         Debug.Log("Beam Attack has Executed!");

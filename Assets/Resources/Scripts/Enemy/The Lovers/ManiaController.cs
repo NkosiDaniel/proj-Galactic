ec0e22c9d3b5c5a15acb6e83b319b55d12938f7f
Ayinde Abrams <xxxenouniverse@gmail.com>
2024-11-11 21:48:30 -0500

    Finished Mania's basic functionality; Next is it's special ability

--- Assets/Resources/Scripts/Enemy/The Lovers/ManiaController.cs
+++ Assets/Resources/Scripts/Enemy/The Lovers/ManiaController.cs
@@ -1,8 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using COMMAND;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class ManiaController : MonoBehaviour
+public class ManiaController : Enemy
 {
+
+    private int attackCounter = 0;
+    private Command strongAttack;
+    private Command weakAttack;
+    [Header("Alternate Attack Info")]
+    [SerializeField] private GameObject secProjectile;
+    [SerializeField] private List<GameObject> secShootOrigins;
+    private void Start()
+    {
+        //HEALTH STATS
+        CurrentHealth = HealthBar.Count;
+        MaxHealth = HealthBar.Count;
+
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        strongAttack = new ShootCommand(fireSpeed, fireRate * 1.2f, projectilePrefab, shootOrigins);
+        weakAttack = new ShootCommand(fireSpeed, fireRate, secProjectile, secShootOrigins);
+    }
+
+    private void Update()
+    {
+        Attack();
+    }
+
+    public override void Attack()
+    {
+        attackCounter++;
+        secShootOrigins[0].transform.LookAt(player);
+        weakAttack.Execute();
+
+        if (attackCounter == 3)
+        {
+            shootOrigins[0].transform.LookAt(player);
+            strongAttack.Execute();
+            attackCounter = 0;
+        }
+    }
+
     
 }

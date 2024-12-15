0cdf74b0f729d7247a038b16d7ae2538ef9f59a3
Ayinde Abrams <xxxenouniverse@gmail.com>
2024-12-15 13:29:40 -0500

    Added Philautia healing factor (this now completes the Lovers series)

--- Assets/Resources/Scripts/Enemy/The Lovers/PhilautiaController.cs
+++ Assets/Resources/Scripts/Enemy/The Lovers/PhilautiaController.cs
@@ -2,17 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PhilautiaController : MonoBehaviour
+public class PhilautiaController : Enemy
 {
-    // Start is called before the first frame update
+    [SerializeField] private List<GameObject> healingSet;
+    private bool threshold = true;
+
     void Start()
     {
-        
+        CurrentHealth = HealthBar.Count;
+        MaxHealth = HealthBar.Count;
+
+        healingSet = new List<GameObject>();
+        StartCoroutine(Heal());
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerStay(Collider other)
     {
-        
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            if (!healingSet.Contains(other.gameObject))
+                healingSet.Add(other.gameObject);
+        }
+        if (healingSet.Count > 0 && threshold)
+        {
+            StartCoroutine(Heal());
+            threshold = false;
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            healingSet.Remove(other.gameObject);
+        }
+        if (healingSet.Count <= 0 && !threshold)
+        {
+            StopCoroutine(Heal());
+            threshold = true;
+        }
+    }
+
+    private IEnumerator Heal()
+    {
+        Debug.Log("Start Coroutine!");
+        while (true)
+        {
+            foreach (GameObject i in healingSet)
+            {
+                Debug.Log(i);
+                i.GetComponent<Enemy>().Push();
+            }
+            yield return new WaitForSeconds(1f);
+        }
+    }
+    public override void Attack()
+    {
+
     }
 }

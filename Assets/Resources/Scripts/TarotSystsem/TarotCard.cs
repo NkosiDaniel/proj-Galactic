d784b3f06a6c97c1d75cba46751ce707689f14a8
Ayinde Abrams <xxxenouniverse@gmail.com>
2025-01-07 21:14:04 -0500

    Revamped Main Menu Screen

---
+++ Assets/Resources/Scripts/TarotSystsem/TarotCard.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TarotCard : ScriptableObject
+{
+    //Rarity coefficient
+    private float rarityCoeff;
+    private bool reroll;
+
+    private void Execute() 
+    {
+
+    } 
+}
